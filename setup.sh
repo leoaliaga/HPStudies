49107c672efc4556d62fdecf53a8db229e14bcb5
Aliaga Soplin <leoaliaga@gmail.com>
2023-07-05 16:16:23 -0500

    Updating setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
+source /cvmfs/larsoft.opensciencegrid.org/products/setup
+setup cmake v3_14_3
+setup geant4 v4_10_3_p03b -q e15:prof
+setup fftw v3_3_6_pl2 -q prof
+setup python v2_7_14
+setup dk2nu v01_05_01b -q e15:prof
+unset G4BIN
+
+SETUPDIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+export G4WORKDIR=`dirname ${SETUPDIR}`
+export LD_LIBRARY_PATH=${G4WORKDIR}/build:${G4WORKDIR}/ana:${LD_LIBRARY_PATH}
+
+#cmake .
+#make all
+
+
